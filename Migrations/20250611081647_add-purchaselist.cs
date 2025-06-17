c0f1f4ab9291ea1dd9f5ecb5e190d937a4e78413
caizhen <zhenchai0000@gmail.com>
2025-06-17 18:10:44 +0800

    Add: db migration and update appsettings for db connection

---
+++ Migrations/20250611081647_add-purchaselist.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCFlowerShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class addpurchaselist : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PurchaseList",
+                columns: table => new
+                {
+                    PurchaseId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PurchaseAmount = table.Column<double>(type: "float", nullable: false),
+                    ItemQuantity = table.Column<int>(type: "int", nullable: false),
+                    PurchaseDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PurchaseList", x => x.PurchaseId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PurchaseList");
+        }
+    }
+}
